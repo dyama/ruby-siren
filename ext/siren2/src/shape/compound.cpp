2209593d3c09ef475c0361260bdf981cbb9ff6e7
dyama <dyama@member.fsf.org>
2017-03-19 23:20:38 +0900

    Update Siren::Compound#push method.

--- ext/siren2/src/shape/compound.cpp
+++ ext/siren2/src/shape/compound.cpp
@@ -21,19 +21,14 @@ VALUE siren_compound_init(int argc, VALUE* argv, VALUE self)
 {
   VALUE a;
   rb_scan_args(argc, argv, "*", &a);
-
   TopoDS_Compound comp;
   BRep_Builder B;
   B.MakeCompound(comp);
-
-  rb_funcall(a, rb_intern("flatten!"), 0);
-
-  for (int i = 0; i < RARRAY_LEN(a); i++) {
-    VALUE item = RARRAY_AREF(a, i);
-    TopoDS_Shape* shape = siren_shape_get(item);
+  VALUE b = rb_funcall(a, rb_intern("flatten"), 0);
+  for (int i = 0; i < RARRAY_LEN(b); i++) {
+    TopoDS_Shape* shape = siren_shape_get(RARRAY_AREF(b, i));
     B.Add(comp, *shape);
   }
-
   auto p = siren_shape_get(self);
   *p = comp;
   return self;
@@ -41,48 +36,28 @@ VALUE siren_compound_init(int argc, VALUE* argv, VALUE self)
 
 VALUE siren_compound_push(int argc, VALUE* argv, VALUE self)
 {
-  VALUE* a;
-  VALUE len;
-  rb_scan_args(argc, argv, "*", &a, &len);
+  VALUE a;
+  rb_scan_args(argc, argv, "*", &a);
   TopoDS_Compound comp = siren_compound_get(self);
   BRep_Builder B;
-  for (int i = 0; i < len; i++) {
-    VALUE arg = *(a + i);
-    if (rb_array_p(arg)) {
-      VALUE subary = rb_funcall(arg, rb_intern("flatten"), 0);
-      for (int j = 0; j < RARRAY_LEN(subary); j++) {
-        TopoDS_Shape* shape = siren_shape_get(RARRAY_AREF(subary, j));
-        B.Add(comp, *shape);
-      }
-    }
-    else {
-      TopoDS_Shape* shape = siren_shape_get(arg);
-      B.Add(comp, *shape);
-    }
+  VALUE b = rb_funcall(a, rb_intern("flatten"), 0);
+  for (int i = 0; i < RARRAY_LEN(b); i++) {
+    TopoDS_Shape* shape = siren_shape_get(RARRAY_AREF(b, i));
+    B.Add(comp, *shape);
   }
   return self;
 }
 
 VALUE siren_compound_delete(int argc, VALUE* argv, VALUE self)
 {
-  VALUE* a;
-  VALUE len;
-  rb_scan_args(argc, argv, "*", &a, &len);
+  VALUE a;
+  rb_scan_args(argc, argv, "*", &a);
   TopoDS_Compound comp = siren_compound_get(self);
   BRep_Builder B;
-  for (int i = 0; i < len; i++) {
-    VALUE arg = *(a + i);
-    if (rb_array_p(arg)) {
-      VALUE subary = rb_funcall(arg, rb_intern("flatten"), 0);
-      for (int j = 0; j < RARRAY_LEN(subary); j++) {
-        TopoDS_Shape* shape = siren_shape_get(RARRAY_AREF(subary, j));
-        B.Remove(comp, *shape);
-      }
-    }
-    else {
-      TopoDS_Shape* shape = siren_shape_get(arg);
-      B.Remove(comp, *shape);
-    }
+  VALUE b = rb_funcall(a, rb_intern("flatten"), 0);
+  for (int i = 0; i < RARRAY_LEN(b); i++) {
+    TopoDS_Shape* shape = siren_shape_get(RARRAY_AREF(b, i));
+    B.Remove(comp, *shape);
   }
   return self;
 }
